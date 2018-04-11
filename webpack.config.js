453a7fced056336121952236612c19b78d2ee4bb
Anurag Khare <theanuragkhare@gmail.com>
2018-04-10 23:30:31 -0400

    Updated webpack config to emit prod build

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,37 @@
 const path = require("path");
 const ErrorOverlayPlugin = require("error-overlay-webpack-plugin");
 
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, "public"),
-    historyApiFallback: true,
-    watchContentBase: true,
-    overlay: true
-  },
-  plugins: [new ErrorOverlayPlugin()],
-  devtool: "cheap-module-eval-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+module.exports = env => {
+  const isProduction = env === "production";
+
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.resolve(__dirname, "public"),
+      filename: "bundle.js"
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, "public"),
+      historyApiFallback: true,
+      watchContentBase: true,
+      overlay: true
+    },
+    plugins: [new ErrorOverlayPlugin()],
+    devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
         }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  }
+      ]
+    }
+  };
 };
